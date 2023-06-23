564b9f3a7df14196ff56786012ead5232505596e
joebanezair <49142653+joebanezair@users.noreply.github.com>
2023-06-24 00:43:24 +0800

    First Commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+import ChangeEmail from './assets/components/email_components';
+import { styles, textStyle } from './assets/components/design_components';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     
+      <ChangeEmail />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
