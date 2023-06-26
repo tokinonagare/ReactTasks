61bc982b81bf487a46e343eeb8bda284560d248f
tokinonagare <timemahou@gmail.com>
2023-06-26 13:16:16 +0800

    添加 当前邮箱账号多语言

--- src/view/UnBindEmailAddress.js
+++ src/view/UnBindEmailAddress.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import styles, { textStyle } from '../style/UnBindEmailAddressStyle';
+import { Localization } from 'laiwan_localization';
 
 const UnBindEmailAddress = ({ emailAddress }) => {
     return (
         <>
-            <Text style={styles.label}>当前邮箱号</Text>
+            <Text style={styles.label}>{Localization.translate('current_email_account')}</Text>
             <View style={styles.emailContainer}>
                 <Text style={[textStyle.text, styles.emailText]}>{emailAddress}</Text>
             </View>
